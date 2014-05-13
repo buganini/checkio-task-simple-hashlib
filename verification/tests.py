04d57185945b00fd7a9e40edf92d7c11789ead4a
showjackyang <showjackyang@gmail.com>
2014-05-13 15:53:28 +0800

    add a little tests

--- verification/tests.py
+++ verification/tests.py
@@ -8,38 +8,54 @@ Each test is dict with
 """
 
 import hashlib
+import random
+from sys import version_info as version
+
+hashfunc_dic = {"md5":hashlib.md5, "sha225": hashlib.sha224, "sha256":hashlib.sha256, "sha256":hashlib.sha256,
+            "sha384":hashlib.sha384, "sha512":hashlib.sha512, "sha1":hashlib.sha1
+            }
 
 
+if version.major == 3: 
+    hash_text = lambda t, func: hashfunc_dic[func](bytes(t, "utf-8")).hexdigest()
+else:
+    hash_text = lambda t, func: hashfunc_dic[func](t).hexdigest()
+
+result_list = []
+for hash_f in hashfunc_dic:
+    for i in range(40):
+        t=""
+        for j in range(1, random.randint(1,100)):
+            t+=chr(random.randint(32,126))
+        result_list.append({"input": (t, hash_f), "answer": hash_text(t, hash_f)})
 
 TESTS = {
     "Basics": [
         {
             "input": ('welcome to pycon','md5'),
-            "answer": hashlib.md5(b'welcome to simple pycon game').hexdigest()
+            "answer": hash_text('welcome to pycon','md5')
         },
         {
             "input": ('welcome to pycon', 'sha224'),
-            "answer": hashlib.sha224(b'welcome to pycon').hexdigest()
+            "answer": hash_text('welcome to pycon','sha224')
         },
         {
             "input": ('welcome to pycon', 'sha256'),
-            "answer": hashlib.sha256(b'welcome to pycon').hexdigest()
+            "answer": hash_text('welcome to pycon','sha256')
         },
         {
             "input": ('welcome to pycon', 'sha384'),
-            "answer": hashlib.sha384(b'welcome to pycon').hexdigest()
+            "answer": hash_text('welcome to pycon','sha384')
         },
         {
             "input": ('welcome to pycon', 'sha512'),
-            "answer": hashlib.sha512(b'welcome to pycon').hexdigest()
+            "answer": hash_text('welcome to pycon','sha512')
         },
         {
             "input": ('welcome to pycon', 'sha1'),
-            "answer": hashlib.sha1(b'welcome to pycon').hexdigest()
+            "answer": hash_text('welcome to pycon','sha1')
         }
 
-
-
-
-    ]
+    ],
+    "Extra": result_list
     }
