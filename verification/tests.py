4aa59ef3cfc64ccac988918a25e9a7c928e4ce73
Buganini Q <buganini@gmail.com>
2014-05-18 14:38:14 +0800

    fix test cases

--- verification/tests.py
+++ verification/tests.py
@@ -11,12 +11,12 @@ import hashlib
 import random
 from sys import version_info as version
 
-hashfunc_dic = {"md5":hashlib.md5, "sha225": hashlib.sha224, "sha256":hashlib.sha256, "sha256":hashlib.sha256,
+hashfunc_dic = {"md5":hashlib.md5, "sha224": hashlib.sha224, "sha256":hashlib.sha256, "sha256":hashlib.sha256,
             "sha384":hashlib.sha384, "sha512":hashlib.sha512, "sha1":hashlib.sha1
             }
 
 
-if version.major == 3: 
+if version.major == 3:
     hash_text = lambda t, func: hashfunc_dic[func](bytes(t, "utf-8")).hexdigest()
 else:
     hash_text = lambda t, func: hashfunc_dic[func](t).hexdigest()
